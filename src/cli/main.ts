30ae8e01281a77f12e8f6ad5ca18eed0ff1eb9b0
agustin22337 <37782178+agustin22337@users.noreply.github.com>
2026-03-18 13:04:36 -0300

    Update gitIgnore and make generate command

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,9 +1,11 @@
 import { analyzeCommand } from "../commands/analyze";
-
+import { generateCommand } from "../commands/analyze";
 const command = process.argv[2];
 
 if (command === "analyze"){
     analyzeCommand();
+} else  if( command === "generate"){
+    generateCommand();
 } else {
     console.log("Unknown command");
 }
--- src/commands/analyze.ts
+++ src/commands/analyze.ts
@@ -4,7 +4,6 @@ import { DocumentationService } from "../services/documentationService";
 import { DocType } from "../utils/docType";
 
 export async function analyzeCommand() {
-
   const connectionString = "postgresql://sga:sga123@localhost:5432/sga_db";
 
   const connector = new PostgresConnector(connectionString);
@@ -15,8 +14,21 @@ export async function analyzeCommand() {
 
   const schema = await loader.loadSchema();
 
-  const docService = new DocumentationService();
+ console.log(schema);
 
+  await connector.disconnect();
+}
+
+export async function generateCommand() {
+  const connectionString = "postgresql://sga:sga123@localhost:5432/sga_db";
+  const connector = new PostgresConnector(connectionString);
+  await connector.connect();
+
+  const loader = new SchemaLoader(connector.getClient());
+
+  const schema = await loader.loadSchema();
+
+  const docService = new DocumentationService();
   docService.generateDoc(schema, DocType.MD, "docs/schema.md");
 
   console.log("\nDocumentation generated successfully.");
